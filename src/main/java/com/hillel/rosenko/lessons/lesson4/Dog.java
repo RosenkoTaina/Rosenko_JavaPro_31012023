6081d01e5f8d59044595b7623d00fb4e5d83d804
Rosenko Taina <rosenko.taya@gmail.com>
2023-02-24 18:56:48 +0200

    Homework 4 (fixed)

--- src/main/java/com/hillel/rosenko/lessons/lesson4/Dog.java
+++ src/main/java/com/hillel/rosenko/lessons/lesson4/Dog.java
@@ -1,42 +1,19 @@
 package com.hillel.rosenko.lessons.lesson4;
 
+
 public class Dog extends Animal {
-    int swimDistance;
-    int runDistance;
-    static int noOfDogs = 0;
-    public Dog(String name, int swimDistance, int runDistance) {
+    private static int dogCount;
+
+    public Dog(String name) {
         super(name);
-        this.swimDistance = swimDistance;
-        this.runDistance = runDistance;
-        System.out.println("Dog " + name);
+        dogCount++;
     }
 
-    public int getSwimDistance() {
-        if (swimDistance < 0 || swimDistance > 10) {
-            throw new IllegalArgumentException("dog cant swim so far");
-        } else return swimDistance;
+    public static int getDogCount() {
+        return dogCount;
     }
-        public int getRunDistance() {
-            if (runDistance < 0 || runDistance > 500) {
-                throw new IllegalArgumentException("dog cant run so far");
-            } else return runDistance;
-        }
-    static {
-        noOfDogs += 1;
-    }
+}
 
-    @Override
-        public void run() {
-        System.out.println(getName() + " run " + getRunDistance() + " meters");
-        }
-
-
-        @Override
-        public void swim() {
-            System.out.println (getName() + " swim " + getSwimDistance() + " meters");
-        }
-
-    }
 
 
 
