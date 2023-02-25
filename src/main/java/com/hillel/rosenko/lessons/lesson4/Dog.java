f3a3190413736b4e4d67fd1055c8216cf8487aa7
Rosenko Taina <rosenko.taya@gmail.com>
2023-02-25 22:07:49 +0200

    Homework 4 (ticket)

--- src/main/java/com/hillel/rosenko/lessons/lesson4/Dog.java
+++ src/main/java/com/hillel/rosenko/lessons/lesson4/Dog.java
@@ -4,8 +4,8 @@ package com.hillel.rosenko.lessons.lesson4;
 public class Dog extends Animal {
     private static int dogCount;
 
-    public Dog(String name) {
-        super(name);
+    public Dog(String name, int maxRunDistance, int maxSwimDistance) throws InvalidObjectParametersException {
+        super(name, maxRunDistance, maxSwimDistance);
         dogCount++;
     }
 
