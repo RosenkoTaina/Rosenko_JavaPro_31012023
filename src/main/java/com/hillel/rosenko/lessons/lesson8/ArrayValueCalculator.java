4504b06d41737c7efe8973ee755a64c050cccd5f
Rosenko Taina <rosenko.taya@gmail.com>
2023-03-13 23:21:11 +0200

    Homework 8

---
+++ src/main/java/com/hillel/rosenko/lessons/lesson8/ArrayValueCalculator.java
@@ -0,0 +1,21 @@
+package com.hillel.rosenko.lessons.lesson8;
+
+public class ArrayValueCalculator {
+  public static int doCalc(String[][] arr) throws MyCustomException {
+    int sum = 0;
+    if (arr.length != 4 || arr[0].length != 4) {
+      throw new MyCustomException("Array size should be 4x4");
+    }
+    for (String[] row : arr) {
+      for (String val : row) {
+        try {
+          sum += Integer.parseInt(val);
+        } catch (NumberFormatException e) {
+          throw new MyCustomException("Invalid value in array: " + val, e);
+        }
+      }
+    }
+    return sum;
+  }
+}
+
